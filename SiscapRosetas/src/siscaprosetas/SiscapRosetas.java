db5cb0ddf05de5a0490565d6a63ed0b38feda993
Abel Pacara <abel.pacara@gmail.com>
2017-09-13 11:30:50 -0400

    se conecta a POSTGRES

--- SiscapRosetas/src/siscaprosetas/SiscapRosetas.java
+++ SiscapRosetas/src/siscaprosetas/SiscapRosetas.java
@@ -5,6 +5,11 @@
  */
 package siscaprosetas;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Administrador
@@ -16,6 +21,51 @@ public class SiscapRosetas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    	
+    	System.out.println("-------- PostgreSQL "
+				+ "JDBC Connection Testing ------------");
+
+		try {
+
+			Class.forName("org.postgresql.Driver");
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("Where is your PostgreSQL JDBC Driver? "
+					+ "Include in your library path!");
+			e.printStackTrace();
+			return;
+
+		}
+
+		System.out.println("PostgreSQL JDBC Driver Registered!");
+
+		Connection connection = null;
+
+		try {
+
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://192.168.132.2:5432/autopista", "postgres",
+					"hlosv");
+			
+			
+			Statement statement = connection.createStatement();
+			statement.executeUpdate("DROP TABLE mytable");
+			
+
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
     }
     
 }
